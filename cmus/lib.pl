0f7f9098dadb30e23b26dcd6696c7dfabfa59737
neyunya <84000070+neyunya@users.noreply.github.com>
2021-06-17 18:46:53 -0300

    Add files via upload

---
+++ cmus/lib.pl
@@ -0,0 +1,43 @@
+/home/haruhi/Music/miku/Hachioji P, Miku Hatsune - Sweet Devil - Eight Mix.mp3
+/home/haruhi/Music/miku/hatsune miku songs to feel old and nostalgic to _ 2007-2011 _ a playlist-gvJECAYgjM8.mp3
+/home/haruhi/Music/miku/Kikuo - 君はできない子.mp3
+/home/haruhi/Music/miku/Mitchie M × OSTER project, 初音ミク『歌の棲む家〜メゾン初音〜』MV-buRFjDSIu4o.mp3
+/home/haruhi/Music/miku/Mitchie M - 初音ミク『暗殺プリンセス』(マンガ風MV)-xgO-uGH7tho.mp3
+/home/haruhi/Music/miku/ryo(supercell) - Sekiranun Graffiti ft. Hastsune Miku-XKOoIAYSkZQ.mp3
+/home/haruhi/Music/miku/Temp/Hachioji P Miku Hatsune - Sweet Devil - Eight Mix.webm
+/home/haruhi/Music/miku/Temp/Kikuo - 君はできない子.webm
+/home/haruhi/Music/miku/Temp/Yasuo - 透明水彩.webm
+/home/haruhi/Music/miku/[VnSharing] How to Sekai Seifuku (World Domination How-To) - Kagamine Rin, Len - Vocaloid vietsub-bwlsG8BYmR8.mp3
+/home/haruhi/Music/miku/Yasuo - 透明水彩.mp3
+/home/haruhi/Music/miku/ピノキオピー - Mei Mei feat. 初音ミク-MKrVBS6pX9I.mp3
+/home/haruhi/Music/miku/ピノキオピー - ありふれたせかいせいふく feat. 初音ミク _ Common World Domination-8Z3TbMBfDM0.mp3
+/home/haruhi/Music/miku/ピノキオピー- おばけのウケねらい feat. 初音ミク _ Ghosts Play to the Audience-M92c6pl10u0.mp3
+/home/haruhi/Music/miku/ピノキオピー - セカイはまだ始まってすらいない feat. 初音ミク _ The World Hasn’t Even Started Yet-1s8NNPgdl5g.mp3
+/home/haruhi/Music/miku/ピノキオピー - はじめまして地球人さん feat. 初音ミク _ Nice To Meet You, Mr. Earthling-e1gY8dUfKxg.mp3
+/home/haruhi/Music/miku/【初音ミク】 恋愛裁判 Love Trial 【オリジナルMV】-TXzfQ0cP1P0.mp3
+/home/haruhi/Music/miku/daniwellP - ねこみみスイッチ.mp3
+/home/haruhi/Music/miku/DiossignalP - リンリンシグナル.mp3
+/home/haruhi/Music/miku/Dixie Flatline - Just Be Friends.mp3
+/home/haruhi/Music/miku/Hachioji P, Miku Hatsune - Sweet Devil - 八王子P VDM Remix.mp3
+/home/haruhi/Music/miku/Hachioji P, Miku Hatsune - 気まぐれメルシィ.mp3
+/home/haruhi/Music/miku/Hikarisyuyo - 孤独の果て.mp3
+/home/haruhi/Music/miku/Hitoshizuku,Yama - Bad ∞ End ∞ Night.mp3
+/home/haruhi/Music/miku/iroha(sasaki) - 炉心融解.mp3
+/home/haruhi/Music/miku/Jin - Kagerou Days.mp3
+/home/haruhi/Music/miku/KuroUsa - 千本桜.mp3
+/home/haruhi/Music/miku/KuroUsa - 紅一葉.mp3
+/home/haruhi/Music/miku/LamazeP - 愛の詩.mp3
+/home/haruhi/Music/miku/livetune, Miku Hatsune - Tell Your World.mp3
+/home/haruhi/Music/miku/livetune, Miku Hatsune - Weekender Girl.mp3
+/home/haruhi/Music/miku/Maretu - Brain Revolution Girl 2016 ver..mp3
+/home/haruhi/Music/miku/PinocchioP - すろぉもぉしょん.mp3
+/home/haruhi/Music/miku/PinocchioP - Last Continue.mp3
+/home/haruhi/Music/miku/PinocchioP - ニナ.mp3
+/home/haruhi/Music/miku/PinocchioP - こあ.mp3
+/home/haruhi/Music/miku/tilt-six - エレクトロサチュレイタ.mp3
+/home/haruhi/Music/miku/toa - ツギハギスタッカート.mp3
+/home/haruhi/Music/miku/tokuP - SPiCa.mp3
+/home/haruhi/Music/miku/DECO27 - 愛言葉 -MIKUNOPOLIS in LOS ANGELES Live-.mp3
+/home/haruhi/Music/miku/Mitchie M - Freely Tomorrow.mp3
+/home/haruhi/Music/miku/DECO27 - Streaming Heart.mp3
+/home/haruhi/Music/miku/yuchaP - Blackjack.mp3
